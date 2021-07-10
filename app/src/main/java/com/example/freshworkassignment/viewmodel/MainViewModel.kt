0ee05cc1892a0a8d74013e3fc7221f5fe7dd749a
ayush.garg2 <ayush.garg2@gaana.com>
2021-07-10 16:16:37 +0530

    added viewmodel

--- app/src/main/java/com/example/freshworkassignment/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/freshworkassignment/viewmodel/MainViewModel.kt
@@ -2,9 +2,34 @@ package com.example.freshworkassignment.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.freshworkassignment.model.GifModel
+import com.example.freshworkassignment.repo.api.MainRepo
+import java.util.ArrayList
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
+    private var mRepo : MainRepo
+    var mUIError = MutableLiveData<String>()
+    var mUIResponse = MutableLiveData<ArrayList<GifModel>>()
 
+    init {
+        mRepo = MainRepo()
 
+        mRepo.mSuccess.observeForever { data ->
+            mUIResponse.value = data
+        }
+
+        mRepo.mError.observeForever { error ->
+            mUIError.value = error.msg
+        }
+    }
+
+    fun getTrendingGifData(offset : Int) {
+        mRepo.getTrendingGifs(offset)
+    }
+
+    fun getSearchGifData(queryString : String, offset : Int) {
+        mRepo.getSearchResultGif(queryString, offset)
+    }
 }
